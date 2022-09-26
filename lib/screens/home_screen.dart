84930d905f90f0e26f7dd7cfe168179bba15fbe4
turbold <turbold@Yalguuns-iMac.local>
2022-09-26 10:32:33 +0800

    firebase auth

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:linkedin/screens/login_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: (Scaffold(
+          body: Column(
+        children: [
+          const Text('HOME'),
+          MaterialButton(
+              onPressed: ((() {
+                Get.to(const LoginScreen());
+              })),
+              child: const Text('go to login'))
+        ],
+      ))),
+    );
+  }
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,3 @@
+const String loginRoute = '/LoginScreen';
+const String verifyRoute = '/VerifyScreen';
+const String homeRoute = '/';
